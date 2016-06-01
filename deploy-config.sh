2355e8123917d5c8010711edb95907c0392adda4
Christoph Herbst <chris.p.herbst@gmail.com>
2016-06-01 12:06:27 +0200

    add deploy-config

---
+++ deploy-config.sh
@@ -0,0 +1,4 @@
+# main config
+PLUGINSLUG="timelimit-add-on-for-badgeos"
+MAINFILE="badgeos-timelimit.php" # this should be the name of your main php file in the wordpress plugin
+SVNUSER="konnektiv" # your svn username
